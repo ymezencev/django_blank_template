b9c8a8b222b5c253da9920f827f7ea056361a22d
Yuri Mezentsev <yurmezence@gmail.com>
2020-12-22 13:22:06 +0300

    db only postgers

--- src/config/settings.py
+++ src/config/settings.py
@@ -62,15 +62,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': os.getenv('DB_NAME'),
-        # 'USER' : os.getenv('DB_USER'),
-        # 'PASSWORD' : os.getenv('DB_PASSWD'),
-        # 'HOST' : '127.0.0.1',
-        # 'PORT' : '5432',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER' : os.getenv('DB_USER'),
+        'PASSWORD' : os.getenv('DB_PASSWD'),
+        'HOST' : os.getenv('DB_HOST'),
+        'PORT' : os.getenv('DB_PORT'),
     }
 }
 
